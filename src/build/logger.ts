9813ab7a542e9a33dd0afbf29d9798d7521aa38d
Sergei Kovalenko <smith404@live.com>
2026-09-05 18:12:57 +0200

    empty build

---
+++ src/build/logger.ts
@@ -0,0 +1,17 @@
+﻿/** biome-ignore-all lint/suspicious/noConsole: Logger functionality */
+
+type TLoggable = string | Error | unknown;
+
+interface ILogger {
+  error: (...data: TLoggable[]) => void;
+  info: (...data: TLoggable[]) => void;
+  log: (...data: TLoggable[]) => void;
+  warn: (...data: TLoggable[]) => void;
+}
+
+export const logger: ILogger = {
+  error: console.error,
+  info: console.info,
+  log: console.log,
+  warn: console.warn,
+};
---
+++ src/build/start.ts
@@ -0,0 +1,40 @@
+﻿import * as fs from "node:fs";
+import { readdir } from "node:fs/promises";
+import { resolve } from "node:path";
+import { logger } from "./logger.js";
+
+const ROOT = resolve(process.cwd());
+const DIST = resolve(ROOT, "dist");
+const APPS = resolve(ROOT, "src/apps");
+
+const listDirectories = async (path: string): Promise<string[]> => {
+  const entries = await readdir(path, { withFileTypes: true });
+  const dirList = entries.filter((entry) => entry.isDirectory());
+
+  return dirList.map((entry) => entry.name);
+};
+
+const createDistFolderSync = (): void => {
+  try {
+    if (fs.existsSync(DIST)) {
+      fs.rmSync(DIST, { recursive: true, force: true });
+    }
+    fs.mkdirSync(DIST);
+  } catch (error: unknown) {
+    logger.error("❌ Cannot create the dist folder.");
+    if (error instanceof Error) {
+      logger.error(error);
+    }
+    throw error;
+  }
+};
+
+const main = async (): Promise<void> => {
+  createDistFolderSync();
+  logger.info(await listDirectories(APPS));
+};
+
+main().catch((error: unknown): void => {
+  logger.error(error);
+  throw error;
+});
