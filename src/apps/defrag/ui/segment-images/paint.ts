50f7fd323b3b14e80f8aa1856964b453805ae279
Sergei Kovalenko <smith404@live.com>
2026-09-21 17:33:21 +0200

    reformatting

--- src/apps/defrag/ui/segment-images/paint.ts
+++ src/apps/defrag/ui/segment-images/paint.ts
@@ -1,6 +1,4 @@
-﻿type TColor = [r: number, g: number, b: number];
-
-/*
+﻿/*
 
 We are going to produce the following square image
 (■ - border color, □ - main color, ▫ - background):
@@ -24,29 +22,33 @@ each cell is represented by 3 integers 0..255
 
 */
 
+// set this one for all the colored segments: 8..64
+const segmentSize: number = 24;
+
+type TColor = { r: number; g: number; b: number };
+
 const compressColorComponent = (c: number): number => {
   // integer, not greater than 255, not less than 0
   return Math.max(Math.min(Math.floor(c), 255), 0);
 };
 const compressColor = (color: TColor): TColor => {
-  return [
-    compressColorComponent(color[0]),
-    compressColorComponent(color[1]),
-    compressColorComponent(color[2]),
-  ];
+  return {
+    r: compressColorComponent(color.r),
+    g: compressColorComponent(color.g),
+    b: compressColorComponent(color.b),
+  };
 };
 const compressSize = (size: number): number => {
   // integer, not greater than 64, not less than 8
   return Math.max(Math.min(Math.floor(size), 64), 8);
 };
 
-export const buildDataArray = (
-  size: number,
+export const buildImageData = (
   color: TColor,
   border: TColor,
-  background: TColor = [255, 255, 255],
+  background: TColor = { r: 255, g: 255, b: 255 },
 ): Uint8ClampedArray => {
-  const s = compressSize(size);
+  const s = compressSize(segmentSize);
   const c = compressColor(color);
   const b = compressColor(border);
   const bkgrnd = compressColor(background);
@@ -54,22 +56,23 @@ export const buildDataArray = (
   // 3 color components per pixel
   const rowLength = s * 3;
   const totalLength = rowLength * s;
-  const result = new Uint8ClampedArray(totalLength);
+  const imageData = new Uint8ClampedArray(totalLength);
 
   // border rows
-  const borderLength = rowLength - 2;
+  const borderLength = rowLength - 6;
   const bottomStart = totalLength - rowLength * 3;
   for (let i = 0; i < borderLength; i = i + 3) {
     // top border
-    result[i] = b[0];
-    result[i + 1] = b[1];
-    result[i + 2] = b[2];
+    imageData[i] = b.r;
+    imageData[i + 1] = b.g;
+    imageData[i + 2] = b.b;
     // bottom border
-    result[bottomStart + i] = b[0];
-    result[bottomStart + i + 1] = b[1];
-    result[bottomStart + i + 2] = b[2];
+    imageData[bottomStart + i] = b.r;
+    imageData[bottomStart + i + 1] = b.g;
+    imageData[bottomStart + i + 2] = b.b;
   }
-  //
+  // main color
+  for (let i = 3; i < )
 
-  return result;
+  return imageData;
 };
